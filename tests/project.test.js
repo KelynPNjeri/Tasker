89d12fa1a889deab8871769666ba66b804614b52
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-03-29 15:02:30 +0300

    Add tests for project.

--- tests/project.test.js
+++ tests/project.test.js
@@ -1,3 +1,11 @@
+import * as Storage from '../src/Storage';
 // TODO: Test Creating of Project.
+describe('Add Project', () => {
+    it('Add new project with a valid project title', () => {
+        
+
+
+    });
+});
 // TODO: Test Editing Project.
 // TODO: Test Project Deletion.
